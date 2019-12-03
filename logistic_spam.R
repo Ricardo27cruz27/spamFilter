b717db2719b497b79a9559c385a822e0a72d1d04
ricardo27cruz27 <ricardo.cruz@cimat.mx>
2019-12-03 00:36:15 -0600

    Se agregan los modelos svm, nb, lasso, ridge, bglm

--- logistic_spam.R
+++ logistic_spam.R
@@ -1,6 +1,45 @@
 ### FILTRO DE SPAM ###
-
+#paquetes necesarios
+library(sqldf)
 library(LaplacesDemon)
+library(corrplot)
+library(caret)
+#funciones utilizadas en el código
+densidades<-function(modelo,dataset,cutoff){
+  #Probabilidades predichas
+  probas<-predict(modelo,dataset,type="response")
+  #Probs predichas por categoria real
+  prob_sr<-probas[which(dataset$spam==1)]
+  prob_nsr<-probas[which(dataset$spam==0)]
+  #Densidades de spam y no spam predichas
+  den_prob_sr<-density(prob_sr,from = 0,to=1)
+  den_prob_nsr<-density(prob_nsr,from=0,to=1)
+  #Punto de corte
+  if(cutoff==0){
+    obs_min<-which.min(abs(den_prob_sr_sw$y-den_prob_nsr_sw$y))
+    cutoff<-den_prob_nsr$x[obs_min]
+  }
+  #plot
+  x11()
+  plot(den_prob_sr,xlim=c(0,1),ylim=c(0,9),main="",xlab="p()",ylab="",col="red")
+  par(new=T)
+  plot(den_prob_nsr,xlim=c(0,1),ylim=c(0,9),main="",xlab="p()",ylab="",col="darkblue")
+  abline(v=cutoff,col="green")
+  legend(x="topright",legend=c("dist spam=1","dist spam=0","punto de corte"),
+         lty=1,col=c("red","darkblue","green"))
+  #clase predicha con el punto de corte
+  clase_predicha<-as.integer(probas>cutoff)
+  #metricas
+  cm<-table(clase_predicha,dataset$spam)
+  cm<-confusionMatrix(cm,positive = "1")
+  print(cm$overall[1])
+  print(cm$byClass[1])
+  print(cm$byClass[2])
+  return(list(clase_predicha=clase_predicha,
+              confussion_matrix=cm,
+              cutoff=cutoff))
+}
+
 
 #Se carga libreria para carga de datos desde github
 library(RCurl)
@@ -27,39 +66,35 @@ names(datos)<-c('make','address','all','3d','our','over','remove','internet','or
 
 names(datos)
 #Variables que son frecuencias de palabras:
-palabras<-datos[,1:48]
+palabras<-datos[,1:54]
 
 #Variable objetivo
 spam<-datos[,"spam"]
 
+#Palabras más frecuentes por categoría
 prueba<-cbind(palabras,spam)
-
 spam1<-sqldf("SELECT * from prueba WHERE spam=1")
 dim(spam1)
 spam0<-sqldf("SELECT * from prueba WHERE spam=0")
 dim(spam0)
-
-wordcloud(spam1[,1:48])
-
-summary(as.factor(apply(spam1, 1, which.max)))
-
+#palabra más frecuente por categoría
 vec1<-apply(spam1[,1:48], 1, which.max)
 sort(summary(as.factor(names(spam1)[vec1])))
-
 vec0<-apply(spam0[,1:48], 1, which.max)
 sort(summary(as.factor(names(spam0)[vec0])))
 
-
-library(corrplot)
+#Conjunto de entrenamiento y prueba
+set.seed(10)
 train<-sample(1:length(spam),size = 3680)
 test=-train
-
 datos_train<-datos[train,]
 datos_test<- datos[test,]
 
+#Correlación de las variables
 x11()
 corrplot(cor(datos), diag = FALSE, order = "FPC",
          tl.pos = "td", tl.cex = 0.5, method = "color", type = "upper")
+
 
 #Regresion logistica
 logistica<-glm(spam~.,
@@ -67,58 +102,33 @@ logistica<-glm(spam~.,
                family = binomial("logit"),
                maxit = 500)
 logistica
-
 #Probabilidades en 0 o 1.
 summary(as.factor(logistica$fitted.values))
 
-length(which(datos_train$spam==1))
-length(which(datos_train$spam==0))
+#FULL CUTOFF FIJO A .5
+prueba_log<-densidades(logistica,
+                       datos_train,
+                       cutoff = .5)
+prueba_log<-densidades(logistica,
+                       datos_test,
+                       cutoff = .5)
 
-train_spam_real<-datos[which(datos_train$spam==1),58]
-train_nospam_real<-datos[which(datos_train$spam==0),58]
+#FULL CUTOFF LIBRE
+prueba_log<-densidades(logistica,
+                       datos_train,
+                       cutoff = 0)
+prueba_log<-densidades(logistica,
+                       datos_test,
+                       cutoff = 0)
 
-
-probas<-invlogit(predict(logistica,datos_train))
-prob_sr<-probas[which(datos_train$spam==1)]
-prob_nsr<-probas[which(datos_train$spam==0)]
-
-den_prob_sr<-density(prob_sr,from = 0,to=1)
-den_prob_nsr<-density(prob_nsr,from=0,to=1)
-
-plot(den_prob_sr)
-plot(den_prob_nsr)
-
-x11()
-plot(den_prob_sr,xlim=c(0,1),ylim=c(0,9),main="",xlab="p()",ylab="",col="red")
-par(new=T)
-plot(den_prob_nsr,xlim=c(0,1),ylim=c(0,9),main="",xlab="p()",ylab="",col="darkblue")
-abline(v=.5,col="green")
-legend(x="topright",legend=c("dist spam=1","dist spam=0","punto de corte"),
-       lty=1,col=c("red","darkblue","green"))
-
-cutoff<-.5
-clase_predicha<-as.integer(probas>cutoff)
-
-library(caret)
-cm.train<-table(clase_predicha,datos_train$spam)
-cm.train<-confusionMatrix(cm.train,positive = "1")
-cm.train$overall[1]
-cm.train$byClass[1]
-cm.train$byClass[2]
-
-
-probas_test<-invlogit(predict(logistica,datos_test))
-
-
-
-seleccion<-step(mod, scope=list(lower=mod, upper=modelo2), direction="forward")
 
 library(MASS)
 library(tidyverse)
-seleccion<-logistica%>%stepAIC(trace = FALSE)
+seleccion<-logistica%>%stepAIC(trace = FALSE,direction = "both")
+#seleccionfwd<-logistica%>%stepAIC(trace = FALSE,direction = "forward")
+
 
 names(seleccion$coefficients)
-dim(seleccion$model)
 aux<-seleccion$formula
 
 log_sw<-glm(aux,
@@ -126,61 +136,146 @@ log_sw<-glm(aux,
             family = binomial("logit"),
             maxit = 500)
 
-probs_sw<-predict(log_sw,datos_train,type="response")
-summary(as.factor(probs_sw))
+#SELECCION CUTOFF LIBRE TRAIN
+prueba_log_sw<-densidades(log_sw,
+                          datos_train,
+                          cutoff = 0)
+#SELECCION CUTOFF LIBRE TEST
+prueba_log_sw<-densidades(log_sw,
+                          datos_test,
+                          cutoff = 0)
 
-prob_sr_sw<-probs_sw[which(datos_train$spam==1)]
-den_prob_sr_sw<-density(prob_sr_sw,from = 0,to=1)
-prob_nsr_sw<-probs_sw[which(datos_train$spam==0)]
-den_prob_nsr_sw<-density(prob_nsr_sw,from = 0,to=1)
 
-
-plot(den_prob_sr_sw)
-plot(den_prob_nsr)
-
-x11()
-plot(den_prob_sr_sw,xlim=c(0,1),ylim=c(0,9),main="",xlab="p()",ylab="",col="red")
-par(new=T)
-plot(den_prob_nsr_sw,xlim=c(0,1),ylim=c(0,9),main="",xlab="p()",ylab="",col="darkblue")
-abline(v=.36,col="green")
-legend(x="topright",legend=c("dist spam=1","dist spam=0","punto de corte"),
-       lty=1,col=c("red","darkblue","green"))
-
-cutoff<-.36
-clase_predicha_sw<-as.integer(probs_sw>cutoff)
-
-cm.train<-table(clase_predicha_sw,datos_train$spam)
-cm.train<-confusionMatrix(cm.train,positive = "1")
-cm.train$overall[1]
-cm.train$byClass[1]
-cm.train$byClass[2]
+# LOS MODELOS FULL Y STEPWISE TIENEN
+# SEPARACION COMPLETA. SE INTENTARA
+# CV, PLS, REGRESION BAYESIANA, SVM,NAIVE
 
 
 
+#lasso:
+library(glmnet)
+# Find the best lambda using cross-validation
+cv.lasso <- cv.glmnet(as.matrix(datos_train[,-58]),
+                      as.matrix(datos_train[,58]),
+                      alpha = 1, family = "binomial")
+x11();plot(cv.lasso)
+# Fit the final model on the training data
+model <- glmnet(as.matrix(datos_train[,-58]),
+                as.matrix(datos_train[,58]),
+                alpha = 1, family = "binomial",
+                lambda = cv.lasso$lambda.min)
+# Display regression coefficients
+coef(model)
+# Make predictions on the test data
+x.test <- model.matrix(spam~., datos_test)[,-1]
+probabilities <- model %>% predict(newx = x.test)
+predicted.classes <- ifelse(probabilities > 0.5, 1, 0)
+# Model accuracy
+observed.classes <- datos_test[,58]
+mean(predicted.classes == observed.classes)
 
 
 
-#test
-probs_sw_ts<-predict(log_sw,datos_test,type="response")
-#summary(as.factor(probs_sw_ts))
+#ridge:
+# Find the best lambda using cross-validation
+cv.ridge <- cv.glmnet(as.matrix(datos_train[,-58]),
+                      as.matrix(datos_train[,58]),
+                      alpha = 0, family = "binomial")
+x11();plot(cv.ridge)
+# Fit the final model on the training data
+model <- glmnet(as.matrix(datos_train[,-58]),
+                as.matrix(datos_train[,58]),
+                alpha = 0, family = "binomial",
+                lambda = cv.ridge$lambda.min)
+# Display regression coefficients
+coef(model)
+# Make predictions on the test data
+x.test <- model.matrix(spam~., datos_test)[,-1]
+probabilities <- model %>% predict(newx = x.test)
+predicted.classes <- ifelse(probabilities > 0.5, 1, 0)
+# Model accuracy
+observed.classes <- datos_test[,58]
+mean(predicted.classes == observed.classes)
 
-prob_sr_sw_ts<-probs_sw_ts[which(datos_test$spam==1)]
-den_prob_sr_sw_ts<-density(prob_sr_sw_ts,from = 0,to=1)
-prob_nsr_sw_ts<-probs_sw_ts[which(datos_test$spam==0)]
-den_prob_nsr_sw_ts<-density(prob_nsr_sw_ts,from = 0,to=1)
 
-x11()
-plot(den_prob_sr_sw_ts,xlim=c(0,1),ylim=c(0,9),main="",xlab="p()",ylab="",col="red")
-par(new=T)
-plot(den_prob_nsr_sw_ts,xlim=c(0,1),ylim=c(0,9),main="",xlab="p()",ylab="",col="darkblue")
-abline(v=.36,col="green")
-legend(x="topright",legend=c("dist spam=1","dist spam=0","punto de corte"),
-       lty=1,col=c("red","darkblue","green"))
+#elastic net:
+# Find the best lambda using cross-validation
+cv.enet <- cv.glmnet(as.matrix(datos_train[,-58]),
+                      as.matrix(datos_train[,58]),
+                      alpha = 0.45, family = "binomial")
+x11();plot(cv.enet)
+# Fit the final model on the training data
+model <- glmnet(as.matrix(datos_train[,-58]),
+                as.matrix(datos_train[,58]),
+                alpha = 0, family = "binomial",
+                lambda = cv.enet$lambda.min)
+# Display regression coefficients
+coef(model)
+# Make predictions on the test data
+x.test <- model.matrix(spam~., datos_test)[,-1]
+probabilities <- model %>% predict(newx = x.test)
+predicted.classes <- ifelse(probabilities > 0.5, 1, 0)
+# Model accuracy
+observed.classes <- datos_test[,58]
+mean(predicted.classes == observed.classes)
 
-clase_predicha_sw_ts<-as.integer(probs_sw_ts>cutoff)
 
-cm.test<-table(clase_predicha_sw_ts,datos_test$spam)
-cm.test<-confusionMatrix(cm.test,positive = "1")
-cm.test$overall[1]
-cm.test$byClass[1]
-cm.train$byClass[2]
+
+
+#SVM
+datos_train2<-datos_train
+datos_train2$spam<-as.factor(datos_train$spam)
+svm <- train(
+  spam ~., data = datos_train2, method = "svmLinear",
+  trControl = trainControl("cv", number = 10),
+  preProcess = c("center","scale")
+)
+# Make predictions on the test data
+predicted.classes <- svm %>% predict(datos_test)
+mean(predicted.classes == observed.classes)
+
+
+svm2 <- train(
+  spam ~., data = datos_train2, method = "svmRadial",
+  trControl = trainControl("cv", number = 10),
+  preProcess = c("center","scale")
+)
+# Make predictions on the test data
+predicted.classes <- svm2 %>% predict(datos_test)
+mean(predicted.classes == observed.classes)
+
+
+svm3 <- train(
+  spam ~., data = datos_train2, method = "svmPoly",
+  trControl = trainControl("cv", number = 10),
+  preProcess = c("center","scale")
+)
+# Make predictions on the test data
+predicted.classes <- svm3 %>% predict(datos_test)
+mean(predicted.classes == observed.classes)
+
+
+
+library(klaR)
+#NAIVE BAYES
+NB <- train(spam ~., data = datos_train2, method = "nb", 
+               trControl = trainControl("cv", number = 10))
+# Make predictions
+predicted.classes <- NB %>% predict(datos_test)
+# Model n accuracy
+mean(predicted.classes == observed.classes)
+
+
+
+#LOGISTIC BAYESIANA
+library(arm)
+mod_bayes<-bayesglm(formula = spam~.,
+                    family=binomial,
+                    data=datos_train)
+
+result_bayes_train<-densidades(mod_bayes,
+                               datos_train,
+                               cutoff = 0)
+result_bayes_test<-densidades(mod_bayes,
+                               datos_test,
+                               cutoff = result_bayes_train$cutoff)
